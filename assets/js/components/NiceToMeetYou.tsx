669940ff54cc30f46c31eb380eca240f4f36b54c
Asa Leon <asaleonbiz@gmail.com>
2023-03-18 10:18:46 +0900

    Changed; hello component to be named index.

--- assets/js/components/NiceToMeetYou.tsx
+++ assets/js/components/NiceToMeetYou.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function NiceToMeetYou() {
 	return (
 		<div className="container">
-			<h3 className="nice-to">This is Nice to meet you component!</h3>
+			<h2 className="nice-to">This is Nice to meet you component!</h2>
 		</div>
 	)
 }
--- assets/js/components/index.tsx
+++ assets/js/components/index.tsx
@@ -4,7 +4,7 @@ import NiceToMeetYou from "./NiceToMeetYou"
 export default function Index() {
 	return (
 		<div className="container">
-			<h2 className="index">This is Index component!</h2>
+			<h1 className="index">This is Index component!</h1>
 			<NiceToMeetYou />
 		</div>
 	)
